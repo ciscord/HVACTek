97a34efb93bef6f670b004a13088df0d7febb41a
Geoffrey Moody <geoff@GM-Macbook.local>
2015-06-17 12:00:32 -0400

    financing changes

--- Add2Cart/Cart View/CartViewController.m
+++ Add2Cart/Cart View/CartViewController.m
@@ -195,13 +195,13 @@
             break;
         }
         case 84:{
-            finacePay = ((totalAmount - totalSavings)/.8975);
-            invest = (finacePay);
+            finacePay = (totalAmount - totalSavings)/.8975 * 0.0144;
+            invest = (totalAmount - totalSavings)/.8975;
             break;
         }
         case 144:{
-            finacePay = (totalAmount - totalSavings)/.909;
-            invest = (finacePay);
+            finacePay = (totalAmount - totalSavings)/.909 * 0.0111;
+            invest = (totalAmount - totalSavings)/.909;
             break;
         }
         default:
