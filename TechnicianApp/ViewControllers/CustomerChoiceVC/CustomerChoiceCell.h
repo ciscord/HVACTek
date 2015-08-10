39e20fd684566ddcff39f7739ea9a951dbb821ba
Dorin <Dorin@Dorins-iMac.local>
2015-08-10 19:42:41 +0300

    no message

---
+++ TechnicianApp/ViewControllers/CustomerChoiceVC/CustomerChoiceCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomerChoiceCell.h
+//  Signature
+//
+//  Created by Dorin on 8/10/15.
+//  Copyright (c) 2015 Unifeyed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomerChoiceCell : UITableViewCell
+
+@end
