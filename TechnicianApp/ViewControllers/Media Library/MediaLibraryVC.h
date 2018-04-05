8c26813472c142ebebe333718e6e86fd6e94ce6e
Maximiliano <smartmaxdev@gmail.com>
2018-04-05 23:31:58 +0800

    #2936 Add EPA logo #resolve

---
+++ TechnicianApp/ViewControllers/Media Library/MediaLibraryVC.h
@@ -0,0 +1,13 @@
+//
+//  PictureLibraryVC.h
+//  HvacTek
+//
+//  Created by dora's Mac on 6/29/16.
+//  Copyright © 2016 Unifeyed. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+@interface PictureLibraryVC : BaseVC
+
+@end
