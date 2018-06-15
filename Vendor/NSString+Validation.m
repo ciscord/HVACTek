3add9b8283466dd2f1edba2e0dd9f1846346fc67
Maximiliano <smartmaxdev@gmail.com>
2018-06-16 04:06:28 +0800

    #3033 App - Receiving "Invalid Email" error #resolve

--- Vendor/NSString+Validation.m
+++ Vendor/NSString+Validation.m
@@ -12,10 +12,7 @@
 
 -(BOOL) isValidEmail
 {
-    BOOL stricterFilter = NO; //
-    NSString *stricterFilterString = @"^[A-Z0-9a-z\\._%+-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}$";
-    NSString *laxString = @"^.+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2}[A-Za-z]*$";
-    NSString *emailRegex = stricterFilter ? stricterFilterString : laxString;
+    NSString *emailRegex = @".+@(.+\\.)+.{2,6}";
     NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
     return [emailTest evaluateWithObject:self];
 }
